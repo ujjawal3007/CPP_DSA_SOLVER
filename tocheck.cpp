a38378b431b52ad6fd8605c666dde86c38a77f75
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-01 21:11:00 -0700

    1-September

---
+++ tocheck.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace  std;
+int main() {
+    int a = 5;
+    int b = 13;
+    int sum = a + b;
+    cout << "The sum of a + b is :" << sum << endl;
+    return 0;
+}
