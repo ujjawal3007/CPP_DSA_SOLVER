16fcd6ba2baaa5daf821902973b7c7a5bcbb8b5b
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-21 22:40:57 -0400

    21-SEPTEMBER-26

---
+++ mergesort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void merge(vector<int>& arr, int st, int mid, int end) {
+    vector<int> temp;
+        int i = st, j = mid + 1;
+        while(i <= mid && j <= end) {
+        if(arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(i <= mid) {
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j <= end) {
+        temp.push_back(arr[j]);
+        j++;
+    }
+        for(int idx = 0; idx < temp.size(); idx++) {
+        arr[idx + st] = temp[idx];
+    }
+}
+void mergeSort(vector<int>& arr, int st, int end) {
+if(st < end) {
+    int mid = st + (end - st) / 2;
+    mergeSort(arr, st, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, st, mid, end);
+}
+}
+int main() {
+    vector<int> arr = {12, 42, 67, 89, 64};
+    mergeSort(arr, 0, arr.size() - 1);
+    for(int val : arr) {
+        cout << "Merge sort of this is : " << val << endl;
+    }
+    return 0;
+}
